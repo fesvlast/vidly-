310c66f97cf78fd08fc11d674e7f1be6ec6fa4c6
Tymofii <fesvlast@gmail.com>
2018-06-25 12:03:30 +0300

    Fix the port var

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ require('./startup/config')();
 require('./startup/validation')();
 require('./startup/prod')(app);
 
-
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 winston.info(`Started on port ${port} ...`);
 const server = app.listen(port);
 
